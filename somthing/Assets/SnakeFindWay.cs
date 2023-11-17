f2390552701650c9cd034659718441ecd01fd867
Việt Nguyễn Thế <83101689+theviet17@users.noreply.github.com>
2023-11-17 15:05:30 +0700

    Update SnakeFindWay.cs

--- somthing/Assets/SnakeFindWay.cs
+++ somthing/Assets/SnakeFindWay.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SnakeFindWay : MonoBehaviour
@@ -62,65 +63,104 @@ public class SnakeFindWay : MonoBehaviour
 
     public List<aSnake> Snakes = new List<aSnake>();
     [SerializeField] private int[,] number;
-    public List<int> lengthOfSnake = new List<int>();
+    public List<int> allIdOfSnake = new List<int>();
     void Start()
     {
         number = new int[10, 10]
         {
-            { 1 ,  1,  0,  0,  3, 3, -1, -1, -1, -1 },
-            { 1 , -1, -1, -1, -1, 3, -1, -1, -1, -1 },
-            { 0 , -1, -1, -1, -1, 3, -1, -1, -1, -1 },
-            { 0 , -1, -1, -1, -1, 0, -1, -1, -1, -1 },
-            { 0 , -1, -1, -1, -1, 4, -1, -1, -1, -1 },
-            { 2 ,  2,  2,  0,  4, 4, -1, -1, -1, -1 },
+            { 1 ,  1,  0,  3, 3 ,   -1, -1, -1, -1, -1 },
+            { 1 , -1, -1, -1, 3 ,   -1, -1, -1, -1, -1 },
+            { 0 , -1, -1, -1, 3 ,   -1, -1, -1, -1, -1 },
+            { 0 , -1, -1, -1, 4 ,   -1, -1, -1, -1, -1 },
+            { 2 ,  2,  2,  4, 4 ,   -1, -1, -1, -1, -1 },
+            
+            { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
             { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
             { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
             { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
             { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
         };
+        LoadSnakeIDFromMatrix();
     }
 
-    
-    public void SetSnakeDate()
+    void LoadSnakeIDFromMatrix()
     {
-        for (int i = 0; i < lengthOfSnake.Count; i++)
+        for (int i = 0; i < 10; i++)
         {
             for (int j = 0; j < 10; j++)
             {
-                for (int k = 0; k < 10; k++)
+                if (number[i, j] != 0 && number[i, j] != -1)
                 {
-                    if (number[j, k].ToString()[0].ToString() == (i+1).ToString())
+                    allIdOfSnake.Add(number[i, j]);
+                }
+            }
+        }
+        allIdOfSnake =  allIdOfSnake.Distinct().ToList();
+        LoadSnakeByIDFromMatrix();
+
+    }
+
+    public void LoadSnakeByIDFromMatrix()
+    {
+        for (int k = 0; k < allIdOfSnake.Count; k++)
+        {
+            List<aTile> tiles = new List<aTile>();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (number[i, j] == allIdOfSnake[k])
                     {
-                        number[j, k] = i + 1;
-                        CreateSnake(i+1);
+                        var aTile = new aTile(i, j);
+                        tiles.Add(aTile);
                     }
                 }
             }
+            var aSnake = new aSnake(tiles);
+            Snakes.Add(aSnake);
         }
     }
     
-    public void CreateSnake(int SnakeID)
-    {
-        
-    }
-    public void CheckDirectionCanMove(int x, int y, int currentX, int currentY, int value)
-    {
-        var _x = currentX + x;
-        var _y = currentY + y;
-        try
-        {
-            if (number[_x, _y] != -1)
-            {
-                if (number[_x, _y] == 0  || number[_x, _y]  == value)
-                {
-                    
-                }
-            }
-            
-        }
-        catch
-        {
-
-        }
-    }
+    // public void SetSnakeDate()
+    // {
+    //     for (int i = 0; i < lengthOfSnake.Count; i++)
+    //     {
+    //         for (int j = 0; j < 10; j++)
+    //         {
+    //             for (int k = 0; k < 10; k++)
+    //             {
+    //                 if (number[j, k].ToString()[0].ToString() == (i+1).ToString())
+    //                 {
+    //                     number[j, k] = i + 1;
+    //                     CreateSnake(i+1);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    // public void CreateSnake(int SnakeID)
+    // {
+    //     
+    // }
+    // public void CheckDirectionCanMove(int x, int y, int currentX, int currentY, int value)
+    // {
+    //     var _x = currentX + x;
+    //     var _y = currentY + y;
+    //     try
+    //     {
+    //         if (number[_x, _y] != -1)
+    //         {
+    //             if (number[_x, _y] == 0  || number[_x, _y]  == value)
+    //             {
+    //                 
+    //             }
+    //         }
+    //         
+    //     }
+    //     catch
+    //     {
+    //
+    //     }
+    // }
 }
